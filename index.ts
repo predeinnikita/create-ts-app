a54e1f27746fa221a260a42a37fc80de1a5261d8
predeinnikita <predein697@gmail.com>
2023-02-26 01:33:25 +0500

    init

---
+++ index.ts
@@ -0,0 +1,33 @@
+import {exec, ExecException} from "child_process";
+
+const templateRepo = 'ts-project-initializer-template';
+const templateVersion = 1;
+
+const commands = [
+    `git clone git@github.com:predeinnikita/${templateRepo}.git`,
+    `cp ${templateRepo}/template-${templateVersion}/. . -R`,
+    `rm ${templateRepo} -R`,
+];
+
+function showMessage(error: ExecException | null, stdout: string, stderr: string) {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+}
+
+function runCommands(commands: string[], commandIndex: number = 0): void {
+    exec(commands[commandIndex], (error, stdout, stderr) => {
+        showMessage(error, stdout, stderr);
+        if (commandIndex !== commands.length - 1) {
+            runCommands(commands, commandIndex + 1)
+        }
+    });
+}
+
+runCommands(commands);
